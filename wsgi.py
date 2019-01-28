66ed2bf2768cca8034a4157abd333a2bef745438
Tobias Patzl <patzl.tobias@gmail.com>
2019-01-28 19:51:09 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return urllib.urlopen("http://www.stackoverflow.com").getcode()
 
 if __name__ == "__main__":
     application.run()
